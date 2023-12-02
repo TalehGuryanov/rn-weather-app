d2553af34188dab618c9a5bd7b3fd292320e5094
Taleh Guryanov <taleh.gurjanov@gmail.com>
2023-12-02 21:57:03 +0400

    added Location

--- App.js
+++ App.js
@@ -1,8 +1,30 @@
-import { View } from 'react-native';
+import { View, Alert } from 'react-native';
+import * as Location from 'expo-location';
 import {Loading} from "./components/Loading";
+import {useEffect, useState} from "react";
 
 export default function App() {
+  const [isLoading, setIsLoading] = useState(true);
+  
+  useEffect(() => {
+    (async () => {
+      
+      const { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        Alert.alert('Permission to access location was denied');
+        return;
+      }
+      
+      try {
+        const {coords: {latitude, longitude}} = await Location.getCurrentPositionAsync({});
+        setIsLoading(false);
+      } catch {
+        Alert.alert('Location not found');
+      }
+    })();
+  }, []);
+  
   return (
-      <Loading />
+      isLoading ? <Loading /> : null
   );
 }
